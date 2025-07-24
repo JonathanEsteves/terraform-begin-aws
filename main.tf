6ac4b5eb5b380d4dca91f46de856644705dcc318
JonathanEsteves <jonathan.dev_93@outlook.com>
2025-07-23 22:21:30 -0300

    vpc:config subnets, cidr and gateway with resources id

--- main.tf
+++ main.tf
@@ -13,7 +13,31 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id = module.vpc.private_subnets[0]
+  
   tags = {
     Name = var.instance_name
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "terraform-vpc"
+  cidr = "10.0.0/16"
+
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_dns_hostnames = true
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
