18517276e01e6666e5819ecf4e1f24860129b3d6
JonathanEsteves <jonathan.dev_93@outlook.com>
2025-07-26 20:09:40 -0300

    hcp: synchronized cli with hcp-terraform

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "vpc" {
   version = "5.19.0"
 
   name = "terraform-vpc"
-  cidr = "10.0.0/16"
+  cidr = "10.0.0.0/16"
 
   azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
@@ -35,7 +35,7 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_nat_gateway = true
   enable_vpn_gateway = true
-  
+
   tags = {
     Terraform = "true"
     Environment = "dev"
--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "terraform-hcp-infra-aws"
+
+    workspaces {
+      project = "Learn Terraform"
+      name = "learn-terraform-aws-get-started"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
